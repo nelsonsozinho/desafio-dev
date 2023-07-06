208dbf17503f9217d5e34ee7ab68afbf288c5cb8
Nelson Sozinho <nelsonsozinho@gmail.com>
2023-07-06 08:16:41 -0400

    migration to create the transaction types

---
+++ back/ktl-cnab/src/main/resources/db/migration/V6__Insert_Transactions.sql
@@ -0,0 +1,10 @@
+insert into cnab_transaction_type (id, type, description, nature, signal, last_modified_at) values('b9c6ac60-2e17-4e66-91c8-890d2f60da63', 1, 'Débito', 'Entrada', '+', now());
+insert into cnab_transaction_type (id, type, description, nature, signal, last_modified_at) values('262aa586-d257-49f0-a87d-0e100a050fdb', 2, 'Boleto', 'Saída', '-', now());
+insert into cnab_transaction_type (id, type, description, nature, signal, last_modified_at) values('d5a2bf6f-2476-47e0-b312-2183f2e824b5', 3, 'Financiamento', 'Saída', '-', now());
+insert into cnab_transaction_type (id, type, description, nature, signal, last_modified_at) values('ee9b9715-cce6-473c-aaec-f099dba1c992', 4, 'Crédito', 'Entrada', '+', now());
+insert into cnab_transaction_type (id, type, description, nature, signal, last_modified_at) values('bb66b200-67c2-4707-b0f4-f40fc3ba9d1e', 5, 'Recebimento Empréstimo', 'Entrada', '+', now());
+insert into cnab_transaction_type (id, type, description, nature, signal, last_modified_at) values('94be7bb9-2636-47bc-9a0b-0f897842f3b7', 6, 'Vendas', 'Entrada', '+', now());
+insert into cnab_transaction_type (id, type, description, nature, signal, last_modified_at) values('dd2673d5-ba63-41e6-abb5-b5109359a2e9', 7, 'Recebimento TED', 'Entrada', '+', now());
+insert into cnab_transaction_type (id, type, description, nature, signal, last_modified_at) values('c8bbf400-7c35-486b-8489-d2e371db140a', 8, 'Recebimento DOC', 'Entrada', '+', now());
+insert into cnab_transaction_type (id, type, description, nature, signal, last_modified_at) values('2c8baed7-f815-4e52-97f2-785feaf592f1', 9, 'Aluguel', 'Saida', '-', now());
+
