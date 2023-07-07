74476685c04973f8c9b68517aa2d6eb9b0394bf4
Nelson Sozinho <nelsonsozinho@gmail.com>
2023-07-07 17:40:14 -0400

    include the Authorization header button in swagger API test

--- back/ktl-cnab/src/main/kotlin/com/shire42/cnab/configuration/SpringFoxConfig.kt
+++ back/ktl-cnab/src/main/kotlin/com/shire42/cnab/configuration/SpringFoxConfig.kt
@@ -2,10 +2,14 @@ package com.shire42.cnab.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.*
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.*
 
 @Configuration
 @EnableSwagger2
@@ -14,10 +18,39 @@ class SpringFoxConfig {
     @Bean
     fun apiDocumentationConfig(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
+            .apiInfo(apiInfo())
+            .securityContexts(listOf(securityContext()))
+            .securitySchemes(listOf(apiKey()))
             .select()
             .apis(RequestHandlerSelectors.basePackage("com.shire42.cnab.controller"))
+            .paths(PathSelectors.any())
             .build()
-            .useDefaultResponseMessages(false)
+    }
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfo(
+            "CNAB REST APIs",
+            "Service to receive ad process transactions from the files",
+            "1",
+            "Terms of service",
+             Contact("Nelson Sozinho", "", "ramesh@gmail.com"),
+        "Apache 2.0",
+        "https://www.apache.org/licenses/LICENSE-2.0.html",
+        Collections.emptyList()
+        )
+    }
+
+    private fun apiKey(): ApiKey {
+        return ApiKey("JWT", "Authorization", "header")
+    }
+    private fun securityContext(): SecurityContext {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+    private fun defaultAuth(): List<SecurityReference> {
+        val scope = AuthorizationScope("global", "accessEverything")
+        val authorizationScopesArrays = arrayOf(scope)
+        val securityReference = SecurityReference("JWT", authorizationScopesArrays);
+        return listOf(securityReference)
     }
 
 }
