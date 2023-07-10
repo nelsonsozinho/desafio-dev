b4a98b4b5cd14d4195a46f02cc626c9ac0ba4889
Nelson Sozinho <nelsonsozinho@gmail.com>
2023-07-10 19:03:35 -0400

    disable button when the file is not choose

--- front/cnab-transactions/src/components/home.component.js
+++ front/cnab-transactions/src/components/home.component.js
@@ -6,192 +6,195 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import TextField from '@mui/material/TextField';
-import React, { Component } from "react";
+import React, {Component} from "react";
 import Form from "react-validation/build/form";
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
 
-import { withRouter } from '../common/with-router';
+import {withRouter} from '../common/with-router';
 
 
 import UserService from "../services/user.service";
 
 class Home extends Component {
 
-  constructor(props) {
-    super(props);
-    this.onChangePropertyNameFilter = this.onChangePropertyNameFilter.bind(this);
-    this.handleTransaction = this.handleTransaction.bind(this);
-    this.handleFileChange = this.handleFileChange.bind(this);
-    this.handleFileUpload = this.handleFileUpload.bind(this);
-    this.state = {
-      content: "",
-      propertyNameFilter: "",
-      file: ""
-    };
+    constructor(props) {
+        super(props);
+        this.onChangePropertyNameFilter = this.onChangePropertyNameFilter.bind(this);
+        this.handleTransaction = this.handleTransaction.bind(this);
+        this.handleFileChange = this.handleFileChange.bind(this);
+        this.handleFileUpload = this.handleFileUpload.bind(this);
+        this.state = {
+            content: "",
+            propertyNameFilter: "",
+            file: null
+        };
 
-  }
+    }
 
-  componentDidMount() {
-    UserService.getAllTransactions().then(
-      response => {
-        this.setState({
-          content: response.data
-        });
-      },
-      error => {
-        this.setState({
-          content:
-            (error.response && error.response.data) ||
-            error.message ||
-            error.toString()
-        });
-      }
-    );
-  }
+    componentDidMount() {
+        UserService.getAllTransactions().then(
+            response => {
+                this.setState({
+                    content: response.data
+                });
+            },
+            error => {
+                this.setState({
+                    content:
+                        (error.response && error.response.data) ||
+                        error.message ||
+                        error.toString()
+                });
+            }
+        );
+    }
 
-  onChangePropertyNameFilter(e) {
-    this.setState(
-      {
-        propertyNameFilter: e.target.value
-      }
-    );
-  }
+    onChangePropertyNameFilter(e) {
+        this.setState(
+            {
+                propertyNameFilter: e.target.value
+            }
+        );
+    }
 
-  handleFileChange(e) {
-      this.setState({file: e.target.files[0]})
-  }
-
-
-  handleFileUpload(e) {
-    UserService.fileUpload(this.state.file).then(
-      response => {
-        this.setState({
-          content: response
-        });
-      },
-      error => {
-        this.setState({
-          content:
-            (error.response && error.response.data) ||
-            error.message ||
-            error.toString()
-        });
-      }
-    );
-  }
+    handleFileChange(e) {
+        this.setState({file: e.target.files[0]})
+    }
 
 
-  handleTransaction(e) {
-    e.preventDefault();
-    UserService.filterTransactions(this.state.propertyNameFilter).then(
-      response => {
-        this.setState({
-          content: response.data
-        });
-      },
-      error => {
-        this.setState({
-          content:
-            (error.response && error.response.data) ||
-            error.message ||
-            error.toString()
-        });
-      }
-    );
-  }
+    handleFileUpload(e) {
+        UserService.fileUpload(this.state.file).then(
+            response => {
+                this.setState({
+                    content: response
+                });
+            },
+            error => {
+                this.setState({
+                    content:
+                        (error.response && error.response.data) ||
+                        error.message ||
+                        error.toString()
+                });
+            }
+        );
+    }
 
-  formatValue(value) {
-    return new Intl.NumberFormat('pt-BR', {
-      style: 'currency',
-      currency: 'BRL'
-    }).format(value);
-  }
 
-  render() {
-    return (
-      <div className="container">
-        <header className="jumbotron">
-          <h3>Transactions</h3>
-        </header>
+    handleTransaction(e) {
+        e.preventDefault();
+        UserService.filterTransactions(this.state.propertyNameFilter).then(
+            response => {
+                this.setState({
+                    content: response.data
+                });
+            },
+            error => {
+                this.setState({
+                    content:
+                        (error.response && error.response.data) ||
+                        error.message ||
+                        error.toString()
+                });
+            }
+        );
+    }
 
-        {this.state.content.transactions?.length === 0 && (
-          <div>
-            <ButtonGroup
-              disableElevation
-              variant="contained"
-              aria-label="Disabled elevation buttons"
-            >
-              <TextField  type="file" onChange={this.handleFileChange} />
-              <Button size="large" variant="contained" onClick={this.handleFileUpload}>Upload</Button>
-            </ButtonGroup>
-            
-          </div>
-        )}
+    formatValue(value) {
+        return new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        }).format(value);
+    }
 
-        <Form
-          onSubmit={this.handleTransaction}
-          ref={c => {
-            this.form = c;
-          }}
-        >
-          {this.state.content.transactions?.length > 0 && (
-            <div className="form-group">
-              <TextField
-                value={this.state.propertyNameFilter}
-                onChange={this.onChangePropertyNameFilter}
-                fullWidth
-                label="Property Name"
-                id="propertyNameFilter"
-              />
+    render() {
+        return (
+            <div className="container">
+                <header className="jumbotron">
+                    <h3>Transactions</h3>
+                </header>
+
+                {this.state.content.transactions?.length === 0 && (
+                    <div>
+                        <ButtonGroup
+                            disableElevation
+                            variant="contained"
+                            aria-label="Disabled elevation buttons"
+                        >
+                            <TextField type="file" onChange={this.handleFileChange}/>
+
+                            <Button disabled={this.state.file == null ? true : false} size="large" variant="contained"
+                                    onClick={this.handleFileUpload}>Upload</Button>
+
+                        </ButtonGroup>
+
+                    </div>
+                )}
+
+                <Form
+                    onSubmit={this.handleTransaction}
+                    ref={c => {
+                        this.form = c;
+                    }}
+                >
+                    {this.state.content.transactions?.length > 0 && (
+                        <div className="form-group">
+                            <TextField
+                                value={this.state.propertyNameFilter}
+                                onChange={this.onChangePropertyNameFilter}
+                                fullWidth
+                                label="Property Name"
+                                id="propertyNameFilter"
+                            />
+                        </div>
+                    )}
+                </Form>
+
+                {this.state.content.transactions?.length && (
+                    <div>
+                        <div>
+                            <p><b>Total:</b> {this.formatValue(this.state.content.transactionAmount)}</p>
+                        </div>
+                        <TableContainer component={Paper}>
+                            <Table sx={{minWidth: 650}} aria-label="simple table">
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell>Owner Name</TableCell>
+                                        <TableCell align="right">Property Name</TableCell>
+                                        <TableCell align="right">Value</TableCell>
+                                        <TableCell align="right">Date</TableCell>
+                                        <TableCell align="right">T. Type</TableCell>
+                                    </TableRow>
+                                </TableHead>
+
+                                <TableBody>
+                                    {this.state.content.transactions && this.state.content.transactions.map((content) => (
+                                        <TableRow
+                                            key={content.id}
+                                            sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                                        >
+                                            <TableCell component="th" scope="row">
+                                                {content.ownerName}
+                                            </TableCell>
+                                            <TableCell align="right">{content.storeName}</TableCell>
+                                            <TableCell align="right">{content.value}</TableCell>
+                                            <TableCell align="right">{content.data}</TableCell>
+                                            <TableCell align="right">{content.nature}</TableCell>
+                                        </TableRow>
+                                    ))}
+                                </TableBody>
+
+                            </Table>
+                        </TableContainer>
+
+                    </div>
+                )}
+
             </div>
-          )}
-        </Form>
-
-        {this.state.content.transactions?.length && (
-        <div>
-          <div>
-            <p><b>Total:</b> {this.formatValue(this.state.content.transactionAmount)}</p>
-          </div>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-              <TableHead>
-                <TableRow>
-                  <TableCell>Owner Name</TableCell>
-                  <TableCell align="right">Property Name</TableCell>
-                  <TableCell align="right">Value</TableCell>
-                  <TableCell align="right">Date</TableCell>
-                  <TableCell align="right">T. Type</TableCell>
-                </TableRow>
-              </TableHead>
-
-              <TableBody>
-                {this.state.content.transactions && this.state.content.transactions.map((content) => (
-                  <TableRow
-                    key={content.id}
-                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                  >
-                    <TableCell component="th" scope="row">
-                      {content.ownerName}
-                    </TableCell>
-                    <TableCell align="right">{content.storeName}</TableCell>
-                    <TableCell align="right">{content.value}</TableCell>
-                    <TableCell align="right">{content.data}</TableCell>
-                    <TableCell align="right">{content.nature}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-
-            </Table>
-          </TableContainer>
-          
-        </div>
-        )}
-
-      </div>
-    );
-  }
+        );
+    }
 }
 
 export default withRouter(Home);
